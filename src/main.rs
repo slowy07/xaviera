b12d6dc704b6906791e4a380c4e97b7146d42ac0
slowy07 <slowy.arfy@proton.me>
2024-08-05 15:33:27 +0700

    chore: remove interpre on main.rs
    Signed-off-by: slowy07 <slowy.arfy@proton.me>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use ast::Node;
 use codegen::CodeGen;
 use lexer::Literal;
 
@@ -7,47 +6,7 @@ mod codegen;
 mod lexer;
 mod parser;
 
-fn interpre(node: Node) -> i64 {
-    match node {
-        Node::LiteralExpr { value } => match value {
-            Literal::Integer(i) => i as i64,
-        },
-        Node::BinaryExpr {
-            left,
-            operator,
-            right,
-        } => {
-            let left = interpre(*left);
-            let right = interpre(*right);
-
-            match operator.token_type {
-                lexer::TokenType::Add => left + right,
-                lexer::TokenType::Sub => left - right,
-                lexer::TokenType::Mul => left * right,
-                lexer::TokenType::Div => left / right,
-                _ => panic!("unexpected token {:?}", operator)
-            }
-        }
-
-        Node::UnaryExpr { operator, right } => {
-            let right = interpre(*right);
-
-            match operator.token_type {
-                lexer::TokenType::Sub => -right,
-                _ => panic!("unexpected token {:?}", operator),
-            }
-        }
-    }
-}
-
 fn main() {
-    let source = String::from("2 + 3 + 4 * 5 / 2;");
+    let source = String::from("2 + 4 * 3 / 2;");
     let mut lexer = lexer::Lexer::new(source);
-    let tokens = lexer.scan_tokens();
-    let mut parser = parser::Parser::new(tokens.clone());
-    let nodes = parser.parse();
-    let mut codegen = CodeGen::new(nodes.clone());
-    let assembly = codegen.generate();
-
-    println!("{}", assembly);
 }
